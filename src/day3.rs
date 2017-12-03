f1a0618498462748dcb5ed97242b6ed9752c66d2
Vincent Lee <vincent.lee@outlook.com>
2017-12-03 00:47:19 -0600

    3-2, super ugly simulation

--- src/day3.rs
+++ src/day3.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::vec::Vec;
 
 const INPUT: u32 = 347991;
@@ -67,6 +68,84 @@ fn part_1() {
     println!("part 1: {}", ring + off);
 }
 
+const UP: (i32, i32) = (0, 1);
+const DOWN: (i32, i32) = (0, -1);
+const LEFT: (i32, i32) = (-1, 0);
+const RIGHT: (i32, i32) = (1, 0);
+
+fn turn_ccw(dir: (i32, i32)) -> (i32, i32) {
+    match dir {
+        UP => LEFT,
+        LEFT => DOWN,
+        DOWN => RIGHT,
+        RIGHT => UP,
+        _ => panic!("unknown direction vector {:?}", dir)
+    }
+}
+
+fn offset(p: (i32, i32), dir: (i32, i32)) -> (i32, i32) {
+    return (p.0 + dir.0, p.1 + dir.1)
+}
+
+fn part_2() {
+    let mut grid: HashMap<(i32, i32), u32> = HashMap::new();
+    grid.insert((0, 0), 1);
+    let mut cur_pos = (1, 0);
+    let mut cur_direction = UP;
+    let mut cur_steps_until_turn = 1u32;
+    let mut cur_side = 0;
+
+    let mut steps_until_turn = 1;
+
+    loop {
+        // fill in spot at cur_pos
+        let val = grid.get(&offset(cur_pos, UP)).unwrap_or(&0)
+            + grid.get(&offset(cur_pos, DOWN)).unwrap_or(&0)
+            + grid.get(&offset(cur_pos, LEFT)).unwrap_or(&0)
+            + grid.get(&offset(cur_pos, RIGHT)).unwrap_or(&0)
+            + grid.get(&offset(offset(cur_pos, UP), LEFT)).unwrap_or(&0)
+            + grid.get(&offset(offset(cur_pos, UP), RIGHT)).unwrap_or(&0)
+            + grid.get(&offset(offset(cur_pos, DOWN), LEFT)).unwrap_or(&0)
+            + grid.get(&offset(offset(cur_pos, DOWN), RIGHT)).unwrap_or(&0);
+        grid.insert(cur_pos, val);
+        println!("filling {:?} with {}", cur_pos, val);
+        if val > INPUT {
+            println!("part 2: {}", val);
+            break;
+        }
+
+        // move to next pos
+        println!("cursteps {}", cur_steps_until_turn);
+        if cur_steps_until_turn == 0 {
+            // turn
+
+            if cur_side == 3 {
+                // finished a ring, go on to the next one outside
+                cur_pos = offset(cur_pos, RIGHT);
+                cur_direction = UP;
+                cur_side = 0;
+                steps_until_turn += 2;
+                cur_steps_until_turn = steps_until_turn;
+                println!("finished a ring, moving to {:?}", cur_pos);
+            } else {
+                // start the next side
+                cur_side += 1;
+                cur_direction = turn_ccw(cur_direction);
+                cur_steps_until_turn = steps_until_turn;
+                println!("turned to face {:?}", cur_direction);
+                cur_pos = offset(cur_pos, cur_direction);
+            }
+        } else {
+            // move along a side
+            cur_steps_until_turn -= 1;
+            cur_pos = offset(cur_pos, cur_direction);
+            println!("moved to {:?}", cur_pos);
+        }
+    }
+
+}
+
 pub fn run() {
     part_1();
+    part_2();
 }
