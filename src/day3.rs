bf281dec3f29e7c3c141228571ade8a27511f975
Vincent Lee <vincent.lee@outlook.com>
2017-12-03 01:10:41 -0600

    More type safety

--- src/day3.rs
+++ src/day3.rs
@@ -1,11 +1,6 @@
 use std::collections::HashMap;
-use std::vec::Vec;
 
 const INPUT: u32 = 347991;
-
-fn ring_to_sidelen(ring: u32) -> u32 {
-    (2*ring + 1)
-}
 
 fn ring_to_ringmax(ring: u32) -> u32 {
     (2*ring + 1) * (2*ring + 1)
@@ -68,30 +63,43 @@ fn part_1() {
     println!("part 1: {}", ring + off);
 }
 
-const UP: (i32, i32) = (0, 1);
-const DOWN: (i32, i32) = (0, -1);
-const LEFT: (i32, i32) = (-1, 0);
-const RIGHT: (i32, i32) = (1, 0);
+#[derive(Debug)]
+enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
 
-fn turn_ccw(dir: (i32, i32)) -> (i32, i32) {
-    match dir {
-        UP => LEFT,
-        LEFT => DOWN,
-        DOWN => RIGHT,
-        RIGHT => UP,
-        _ => panic!("unknown direction vector {:?}", dir)
+impl Direction {
+    fn ccw(&self) -> Direction {
+        match self {
+            &Direction::UP => Direction::LEFT,
+            &Direction::LEFT => Direction::DOWN,
+            &Direction::DOWN => Direction::RIGHT,
+            &Direction::RIGHT => Direction::UP,
+        }
     }
 }
 
-fn offset(p: (i32, i32), dir: (i32, i32)) -> (i32, i32) {
-    return (p.0 + dir.0, p.1 + dir.1)
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
+struct Position(i32, i32);
+impl Position {
+    fn offset(&self, dir: &Direction) -> Position {
+        match dir {
+            &Direction::UP => Position(self.0, self.1 + 1),
+            &Direction::DOWN => Position(self.0, self.1 - 1),
+            &Direction::LEFT => Position(self.0 - 1, self.1),
+            &Direction::RIGHT => Position(self.0 + 1, self.1),
+        }
+    }
 }
 
 fn part_2() {
-    let mut grid: HashMap<(i32, i32), u32> = HashMap::new();
-    grid.insert((0, 0), 1);
-    let mut cur_pos = (1, 0);
-    let mut cur_direction = UP;
+    let mut grid: HashMap<Position, u32> = HashMap::new();
+    grid.insert(Position(0, 0), 1);
+    let mut cur_pos = Position(1, 0);
+    let mut cur_direction = Direction::UP;
     let mut cur_steps_until_turn = 1u32;
     let mut cur_side = 0;
 
@@ -99,14 +107,14 @@ fn part_2() {
 
     loop {
         // fill in spot at cur_pos
-        let val = grid.get(&offset(cur_pos, UP)).unwrap_or(&0)
-            + grid.get(&offset(cur_pos, DOWN)).unwrap_or(&0)
-            + grid.get(&offset(cur_pos, LEFT)).unwrap_or(&0)
-            + grid.get(&offset(cur_pos, RIGHT)).unwrap_or(&0)
-            + grid.get(&offset(offset(cur_pos, UP), LEFT)).unwrap_or(&0)
-            + grid.get(&offset(offset(cur_pos, UP), RIGHT)).unwrap_or(&0)
-            + grid.get(&offset(offset(cur_pos, DOWN), LEFT)).unwrap_or(&0)
-            + grid.get(&offset(offset(cur_pos, DOWN), RIGHT)).unwrap_or(&0);
+        let val = grid.get(&cur_pos.offset(&Direction::UP)).unwrap_or(&0)
+            + grid.get(&cur_pos.offset(&Direction::DOWN)).unwrap_or(&0)
+            + grid.get(&cur_pos.offset(&Direction::LEFT)).unwrap_or(&0)
+            + grid.get(&cur_pos.offset(&Direction::RIGHT)).unwrap_or(&0)
+            + grid.get(&cur_pos.offset(&Direction::UP).offset(&Direction::LEFT)).unwrap_or(&0)
+            + grid.get(&cur_pos.offset(&Direction::UP).offset(&Direction::RIGHT)).unwrap_or(&0)
+            + grid.get(&cur_pos.offset(&Direction::DOWN).offset(&Direction::LEFT)).unwrap_or(&0)
+            + grid.get(&cur_pos.offset(&Direction::DOWN).offset(&Direction::RIGHT)).unwrap_or(&0);
         grid.insert(cur_pos, val);
         println!("filling {:?} with {}", cur_pos, val);
         if val > INPUT {
@@ -121,8 +129,8 @@ fn part_2() {
 
             if cur_side == 3 {
                 // finished a ring, go on to the next one outside
-                cur_pos = offset(cur_pos, RIGHT);
-                cur_direction = UP;
+                cur_pos = cur_pos.offset(&Direction::RIGHT);
+                cur_direction = Direction::UP;
                 cur_side = 0;
                 steps_until_turn += 2;
                 cur_steps_until_turn = steps_until_turn;
@@ -130,15 +138,15 @@ fn part_2() {
             } else {
                 // start the next side
                 cur_side += 1;
-                cur_direction = turn_ccw(cur_direction);
+                cur_direction = cur_direction.ccw();
                 cur_steps_until_turn = steps_until_turn;
                 println!("turned to face {:?}", cur_direction);
-                cur_pos = offset(cur_pos, cur_direction);
+                cur_pos = cur_pos.offset(&cur_direction);
             }
         } else {
             // move along a side
             cur_steps_until_turn -= 1;
-            cur_pos = offset(cur_pos, cur_direction);
+            cur_pos = cur_pos.offset(&cur_direction);
             println!("moved to {:?}", cur_pos);
         }
     }
