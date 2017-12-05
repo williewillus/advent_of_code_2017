28bb076dcd41f28fe57507f35f12faf6eb307c9d
Vincent Lee <vincent.lee@outlook.com>
2017-12-04 23:13:02 -0600

    d5

---
+++ src/day5.rs
@@ -0,0 +1,39 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+pub fn run() {
+    let f = File::open("d5_input.txt").unwrap();
+
+    let mut nums = BufReader::new(f)
+        .lines()
+        .map(|l| l.unwrap().parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+
+    compute(&mut nums.clone(), false); // need clean copy since it destructively mutates
+    compute(&mut nums, true);
+}
+
+fn compute(nums: &mut [i32], part_2: bool) {
+    let mut pc = 0i32;
+    let mut steps = 0;
+
+    while pc >= 0 && (pc as usize) < nums.len() {
+        let insn = nums[pc as usize];
+        if part_2 && insn >= 3 {
+            nums[pc as usize] -= 1;
+        } else {
+            nums[pc as usize] += 1;
+        }
+        pc += insn;
+        steps += 1;
+    }
+
+    if part_2 {
+        println!("part 2: {}", steps);
+    } else {
+        println!("part 1: {}", steps);
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 mod util;
 
 fn main() {
@@ -16,6 +17,7 @@ fn main() {
         "2" => day2::run(),
         "3" => day3::run(),
         "4" => day4::run(),
+        "5" => day5::run(),
         _ => panic!("no such day {}", args[1])
     }
 }
