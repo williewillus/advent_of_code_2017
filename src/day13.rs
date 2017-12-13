9d18465266c82a4e740a828ae1c9dc6ec7a435f8
Vincent Lee <vincent.lee@outlook.com>
2017-12-12 23:55:55 -0600

    d13, part 2 is broken for whatever reason

---
+++ src/day13.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+fn severity(ranges: &HashMap<u32, u32>, preticks: u32) -> u32 {
+    let mut severity = 0;
+    for (&k, &v) in ranges {
+        // e.g. range 4: 0 1 2 3 2 1 | 0 1 2 ...
+        if (k + preticks) % (2 * (v-1)) == 0 {
+            severity += k * v;
+        }
+    }
+    severity
+}
+
+pub fn run() {
+    let f = File::open("d13_input.txt").unwrap();
+    let mut firewall_ranges = HashMap::new();
+
+    for line in BufReader::new(f).lines().filter_map(|l| l.ok()) {
+        let splits = line.split(": ").collect::<Vec<_>>();
+        let id = splits[0].parse::<u32>().unwrap();
+        let range = splits[1].parse::<u32>().unwrap();
+        firewall_ranges.insert(id, range);
+    }
+
+    println!("part 1: {}", severity(&firewall_ranges, 0));
+
+    for preticks in 1.. {
+        if severity(&firewall_ranges, preticks) == 0 {
+            println!("part 2: {}", preticks);
+            break;
+        }
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod day9;
 mod day10;
 mod day11;
 mod day12;
+mod day13;
 mod util;
 
 extern crate regex;
@@ -34,6 +35,7 @@ fn main() {
         "10" => day10::run(),
         "11" => day11::run(),
         "12" => day12::run(),
+        "13" => day13::run(),
         _ => panic!("no such day {}", args[1])
     }
 }
