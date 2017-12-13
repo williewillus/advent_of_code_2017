a597a66bff7c5559e1830f2647d850c75741ee49
Vincent Lee <vincent.lee@outlook.com>
2017-12-13 00:14:41 -0600

    Fix d13-2

--- src/day13.rs
+++ src/day13.rs
@@ -4,15 +4,19 @@ use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
-fn severity(ranges: &HashMap<u32, u32>, preticks: u32) -> u32 {
+fn severity(ranges: &HashMap<u32, u32>, preticks: u32) -> (bool, u32) {
     let mut severity = 0;
+    let mut caught = false;
+
     for (&k, &v) in ranges {
         // e.g. range 4: 0 1 2 3 2 1 | 0 1 2 ...
         if (k + preticks) % (2 * (v-1)) == 0 {
             severity += k * v;
+            caught = true;
         }
     }
-    severity
+
+    (caught, severity)
 }
 
 pub fn run() {
@@ -26,10 +30,10 @@ pub fn run() {
         firewall_ranges.insert(id, range);
     }
 
-    println!("part 1: {}", severity(&firewall_ranges, 0));
+    println!("part 1: {}", severity(&firewall_ranges, 0).1);
 
     for preticks in 1.. {
-        if severity(&firewall_ranges, preticks) == 0 {
+        if !severity(&firewall_ranges, preticks).0 {
             println!("part 2: {}", preticks);
             break;
         }
