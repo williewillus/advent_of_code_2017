676be52a9993a544683b53db107803ae61108b41
Vincent Lee <vincent.lee@outlook.com>
2017-12-18 22:59:42 -0600

    not-working d18-2, fix later^tm

--- src/day18.rs
+++ src/day18.rs
@@ -1,76 +1,179 @@
 use std::collections::HashMap;
-use std::collections::HashSet;
-use std::collections::VecDeque;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::Sender;
+use std::sync::mpsc::channel;
+use std::time::Duration;
+use std::thread;
 use regex::Regex;
 
+#[derive(Copy, Clone, Debug)]
 enum Operand {
     Reg(char),
     Imm(i64),
 }
 
+#[derive(Copy, Clone, Debug)]
 enum Insn {
-    Snd(Operand),
-    Set(Operand, Operand),
-    Add(Operand, Operand),
-    Mul(Operand, Operand),
-    Mod(Operand, Operand),
-    Rcv(Operand),
-    Jgz(Operand, Operand),
+    Snd(char),
+    Set(char, Operand),
+    Add(char, Operand),
+    Mul(char, Operand),
+    Mod(char, Operand),
+    Rcv(char),
+    Jgz(char, Operand),
 }
 
-fn get_value(opnd: &str, regs: &HashMap<char, i64>) -> i64 {
+fn parse_opnd(opnd: &str) -> Operand {
     if let Ok(imm) = opnd.parse::<i64>() {
-        println!("imm {}", imm);
-        imm
+        Operand::Imm(imm)
     } else {
-        *regs.get(&opnd.chars().next().unwrap()).unwrap_or(&0)
+        Operand::Reg(opnd.chars().next().unwrap())
     }
 }
 
-fn parse_insn(line: &str, re: &Regex, regs: &mut HashMap<char, i64>, last_played: &mut i64, pc: &mut i64) {
-    if let Some(mats) = re.captures(&line) {
-        println!("{}", &mats[1]);
-        match &mats[1] {
-            "snd" => *last_played = get_value(&mats[2], regs),
-            "set" => {
-                *regs.entry(mats[2].chars().next().unwrap()).or_insert(0) = get_value(&mats[3], regs)
+fn parse_insn(line: &str, re: &Regex) -> Insn {
+    let mats = re.captures(&line).unwrap();
+    let ch = mats[2].chars().next().unwrap();
+
+    match &mats[1] {
+        "snd" => Insn::Snd(ch),
+        "set" => Insn::Set(ch, parse_opnd(&mats[3])),
+        "add" => Insn::Add(ch, parse_opnd(&mats[3])),
+        "mul" => Insn::Mul(ch, parse_opnd(&mats[3])),
+        "mod" => Insn::Mod(ch, parse_opnd(&mats[3])),
+        "rcv" => Insn::Rcv(ch),
+        "jgz" => Insn::Jgz(ch, parse_opnd(&mats[3])),
+        _ => panic!("unknown insn {}", &mats[1]),
+    }
+}
+
+struct State {
+    regs: HashMap<char, i64>,
+    pc: i64,
+    insns: Vec<Insn>,
+    snd_queue: Sender<i64>,
+    rcv_queue: Receiver<i64>,
+    send_count: u32,
+}
+
+impl State {
+    fn get_val(&self, opnd: Operand) -> i64 {
+        match opnd {
+            Operand::Reg(r) => *self.regs.get(&r).unwrap_or(&0),
+            Operand::Imm(i) => i,
+        }
+    }
+
+    fn simulate(&mut self, insn: Insn) -> Option<u32> {
+        // I have no idea why I'm getting mismatch arm type errors so here we go () for everyone
+        // todo figure out how to force the arms to not return anything
+        match insn {
+            Insn::Snd(r) => {
+                self.send_count += 1;
+                self.snd_queue.send(*self.regs.get(&r).unwrap_or(&0)).unwrap();
+                ()
             },
-            "add" => *regs.entry(mats[2].chars().next().unwrap()).or_insert(0) += get_value(&mats[3], regs),
-            "mul" => *regs.entry(mats[2].chars().next().unwrap()).or_insert(0) *= get_value(&mats[3], regs),
-            "mod" => *regs.entry(mats[2].chars().next().unwrap()).or_insert(0) %= get_value(&mats[3], regs),
-            "rcv" => {
-                if get_value(&mats[2], regs) != 0 {
-                    panic!("recovered {}", *last_played); // todo stop abusing panic
-                }
+            Insn::Set(r, o) => {
+                let v = self.get_val(o);
+                self.regs.insert(r, v);
+                ()
             },
-            "jgz" => {
-                if get_value(&mats[2], regs) > 0 {
-                    *pc += get_value(&mats[3], regs);
+            Insn::Add(r, o) => {
+                *self.regs.entry(r).or_insert(0) += self.get_val(o);
+                ()
+            },
+            Insn::Mul(r, o) => {
+                *self.regs.entry(r).or_insert(0) *= self.get_val(o);
+                ()
+            },
+            Insn::Mod(r, o) => {
+                *self.regs.entry(r).or_insert(0) %= self.get_val(o);
+                ()
+            },
+            Insn::Rcv(ch) => {
+                if let Ok(v) = self.rcv_queue.recv_timeout(Duration::new(5, 0)) {
+                    self.regs.insert(ch, v);
                 } else {
-                    *pc += 1;
+                    println!("recv timeout");
+                    return Some(self.send_count);
                 }
+                ()
             },
-            _ => panic!("unknown insn {}", &mats[1]),
+            Insn::Jgz(ch, o) => {
+                if *self.regs.get(&ch).unwrap_or(&0) > 0 {
+                    self.pc += self.get_val(o);
+                } else {
+                    self.pc += 1;
+                }
+                ()
+            }
+        };
+
+        match insn {
+            Insn::Jgz(_, _) => (),
+            _ => self.pc += 1,
         }
-        if &mats[1] != "jgz" {
-            *pc += 1;
-        }
+
+        None
     }
 }
 
 pub fn run() {
     let re = Regex::new(r"(snd|set|add|mul|mod|rcv|jgz) ([a-z]|-?\d+)(?: ([a-z]|-?\d+))?").unwrap();
-    let f = File::open("d18_input.txt").unwrap();
+    let insns = BufReader::new(File::open("d18_input.txt").unwrap()).lines().filter_map(|l| l.ok())
+        .map(|l| parse_insn(&l, &re))
+        .collect::<Vec<_>>();
 
-    let lines = BufReader::new(f).lines().filter_map(|l| l.ok()).collect::<Vec<_>>();
-    let mut regs = HashMap::new();
-    let mut pc = 0i64;
-    let mut last_played = 0;
-    while pc >= 0 && (pc as usize) < lines.len() {
-        parse_insn(&lines[pc as usize], &re, &mut regs, &mut last_played, &mut pc);
-        // println!("{:?}", regs);
-    }
+    let (tx0, rx1) = channel();
+    let (tx1, rx0) = channel();
+
+    let mut prog_0 = State {
+        regs: HashMap::new(),
+        pc: 0,
+        insns: insns.clone(),
+        snd_queue: tx0,
+        rcv_queue: rx0,
+        send_count: 0,
+    };
+
+    let mut prog_1 = State {
+        regs: {
+            let mut r = HashMap::new();
+            r.insert('p', 1);
+            r
+        },
+        pc: 0,
+        insns: insns.clone(),
+        snd_queue: tx1,
+        rcv_queue: rx1,
+        send_count: 0,
+    };
+
+    let p0 = thread::spawn(move || {
+        while prog_0.pc >= 0 && (prog_0.pc as usize) < prog_0.insns.len() {
+            let insn = prog_0.insns[prog_0.pc as usize];
+            if let Some(ex) = prog_0.simulate(insn) {
+                return ex;
+            }
+            println!("p0 sends {}", prog_0.send_count);
+        }
+        return 0;
+    });
+
+    let p1 = thread::spawn(move || {
+        while prog_1.pc >= 0 && (prog_1.pc as usize) < prog_1.insns.len() {
+            let insn = prog_1.insns[prog_1.pc as usize];
+            if let Some(ex) = prog_1.simulate(insn) {
+                return ex;
+            }
+            println!("p1 sends {}", prog_1.send_count);
+        }
+        return 0;
+    });
+
+    println!("{:?}", p0.join());
+    println!("{:?}", p1.join());
 }
