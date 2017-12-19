65d0b94ea7b409eee4468a4e94f090ebc5bad849
Vincent Lee <vincent.lee@outlook.com>
2017-12-19 15:46:42 -0600

    Dirty d18-2, clobbers d18-1 (will restore soon ^tm)

--- src/day18.rs
+++ src/day18.rs
@@ -1,12 +1,8 @@
 use std::collections::HashMap;
+use std::collections::VecDeque;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
-use std::sync::mpsc::Receiver;
-use std::sync::mpsc::Sender;
-use std::sync::mpsc::channel;
-use std::time::Duration;
-use std::thread;
 use regex::Regex;
 
 #[derive(Copy, Clone, Debug)]
@@ -23,7 +19,7 @@ enum Insn {
     Mul(char, Operand),
     Mod(char, Operand),
     Rcv(char),
-    Jgz(char, Operand),
+    Jgz(Operand, Operand),
 }
 
 fn parse_opnd(opnd: &str) -> Operand {
@@ -45,7 +41,7 @@ fn parse_insn(line: &str, re: &Regex) -> Insn {
         "mul" => Insn::Mul(ch, parse_opnd(&mats[3])),
         "mod" => Insn::Mod(ch, parse_opnd(&mats[3])),
         "rcv" => Insn::Rcv(ch),
-        "jgz" => Insn::Jgz(ch, parse_opnd(&mats[3])),
+        "jgz" => Insn::Jgz(parse_opnd(&mats[2]), parse_opnd(&mats[3])),
         _ => panic!("unknown insn {}", &mats[1]),
     }
 }
@@ -54,9 +50,8 @@ struct State {
     regs: HashMap<char, i64>,
     pc: i64,
     insns: Vec<Insn>,
-    snd_queue: Sender<i64>,
-    rcv_queue: Receiver<i64>,
     send_count: u32,
+    blocked: bool,
 }
 
 impl State {
@@ -67,48 +62,53 @@ impl State {
         }
     }
 
-    fn simulate(&mut self, insn: Insn) -> Option<u32> {
-        // I have no idea why I'm getting mismatch arm type errors so here we go () for everyone
-        // todo figure out how to force the arms to not return anything
+    fn terminated(&self) -> bool {
+        self.pc < 0 || (self.pc as usize) >= self.insns.len()
+    }
+
+    fn step(&mut self, snd: &mut VecDeque<i64>, rcv: &mut VecDeque<i64>) {
+        if !self.terminated() {
+            self.simulate(snd, rcv);
+        }
+    }
+
+    fn simulate(&mut self, snd: &mut VecDeque<i64>, rcv: &mut VecDeque<i64>) {
+        let insn = self.insns[self.pc as usize];
+
         match insn {
             Insn::Snd(r) => {
                 self.send_count += 1;
-                self.snd_queue.send(*self.regs.get(&r).unwrap_or(&0)).unwrap();
-                ()
+                snd.push_back(*self.regs.get(&r).unwrap_or(&0));
             },
             Insn::Set(r, o) => {
                 let v = self.get_val(o);
                 self.regs.insert(r, v);
-                ()
             },
             Insn::Add(r, o) => {
                 *self.regs.entry(r).or_insert(0) += self.get_val(o);
-                ()
             },
             Insn::Mul(r, o) => {
                 *self.regs.entry(r).or_insert(0) *= self.get_val(o);
-                ()
             },
             Insn::Mod(r, o) => {
                 *self.regs.entry(r).or_insert(0) %= self.get_val(o);
-                ()
             },
             Insn::Rcv(ch) => {
-                if let Ok(v) = self.rcv_queue.recv_timeout(Duration::new(5, 0)) {
-                    self.regs.insert(ch, v);
+                if rcv.is_empty() {
+                    self.blocked = true;
+                    return; // don't increment pc so this insn will be re-run next iteration
                 } else {
-                    println!("recv timeout");
-                    return Some(self.send_count);
+                    self.blocked = false;
+                    self.regs.insert(ch, rcv.pop_front().unwrap());
                 }
-                ()
             },
-            Insn::Jgz(ch, o) => {
-                if *self.regs.get(&ch).unwrap_or(&0) > 0 {
+            Insn::Jgz(test, o) => {
+                let v = self.get_val(test);
+                if v > 0 {
                     self.pc += self.get_val(o);
                 } else {
                     self.pc += 1;
                 }
-                ()
             }
         };
 
@@ -116,8 +116,6 @@ impl State {
             Insn::Jgz(_, _) => (),
             _ => self.pc += 1,
         }
-
-        None
     }
 }
 
@@ -127,16 +125,12 @@ pub fn run() {
         .map(|l| parse_insn(&l, &re))
         .collect::<Vec<_>>();
 
-    let (tx0, rx1) = channel();
-    let (tx1, rx0) = channel();
-
     let mut prog_0 = State {
         regs: HashMap::new(),
         pc: 0,
         insns: insns.clone(),
-        snd_queue: tx0,
-        rcv_queue: rx0,
         send_count: 0,
+        blocked: false,
     };
 
     let mut prog_1 = State {
@@ -147,33 +141,44 @@ pub fn run() {
         },
         pc: 0,
         insns: insns.clone(),
-        snd_queue: tx1,
-        rcv_queue: rx1,
         send_count: 0,
+        blocked: false,
     };
 
-    let p0 = thread::spawn(move || {
-        while prog_0.pc >= 0 && (prog_0.pc as usize) < prog_0.insns.len() {
-            let insn = prog_0.insns[prog_0.pc as usize];
-            if let Some(ex) = prog_0.simulate(insn) {
-                return ex;
-            }
-            println!("p0 sends {}", prog_0.send_count);
-        }
-        return 0;
-    });
+    let mut p0_rcv_queue = VecDeque::new();
+    let mut p1_rcv_queue = VecDeque::new();
 
-    let p1 = thread::spawn(move || {
-        while prog_1.pc >= 0 && (prog_1.pc as usize) < prog_1.insns.len() {
-            let insn = prog_1.insns[prog_1.pc as usize];
-            if let Some(ex) = prog_1.simulate(insn) {
-                return ex;
-            }
-            println!("p1 sends {}", prog_1.send_count);
-        }
-        return 0;
-    });
+    loop {
+        // run p0 until it can't
+        while !prog_0.blocked && !prog_0.terminated() {
+            let old_snd = prog_0.send_count;
 
-    println!("{:?}", p0.join());
-    println!("{:?}", p1.join());
+            prog_0.step(&mut p1_rcv_queue, &mut p0_rcv_queue);
+
+            // p0 sent so unblock p1
+            if prog_0.send_count != old_snd {
+                prog_1.blocked = false;
+            }
+        }
+
+        // run p1 until it can't
+        while !prog_1.blocked && !prog_1.terminated() {
+            let old_snd = prog_1.send_count;
+
+            prog_1.step(&mut p0_rcv_queue, &mut p1_rcv_queue);
+
+            // p1 sent so unblock p0
+            if prog_1.send_count != old_snd {
+                prog_0.blocked = false;
+            }
+        }
+
+        if (prog_0.blocked && prog_1.blocked)
+            || (prog_0.terminated() && prog_1.terminated()) {
+            break;
+        }
+    } // keep running them cooperatively until they're both done or blocked
+
+    println!("part 2: {}", prog_1.send_count);
+
 }
